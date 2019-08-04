2256fc3eb696e3f0708c32bc927d29c64e037dde
IraSagata <irasagata@gmail.com>
2019-08-04 22:17:00 +0300

    some changes

--- modul-02/task-7.js
+++ modul-02/task-7.js
@@ -4,7 +4,7 @@ let input;
 let numbers = [];
 let total = 0;
 for (let i = 0; i <= numbers.length; i += 1) {
-  input = +prompt("Введи число");
+  input = prompt("Введи число");
   numbers.push(Number(input));
   total += numbers[i];
   if (input === null) {
